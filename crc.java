d3c5ea116eb27ab4c341f2cd78633e43c4d25d3b
Varsha A <99706376+varsha0713@users.noreply.github.com>
2024-01-03 10:13:52 +0530

    commit

--- crc.java
+++ crc.java
@@ -1,73 +1,80 @@
 import java.util.*;
-	public class prg{
-		public static void main(String args[]) {
-			Scanner s=new Scanner(System.in);
-			int n;
-			System.out.println("Enter the sizen of the data:");
-			n=s.nextInt();
-			int data[]=new int[n];
-			System.out.println("Engtre the bit by bit:");
-				for(int i=0;i<n;i++) {
-					data[i]=s.nextInt();
-					}
-			System.out.println("Enter the sizen of the devisor:");
-			n=s.nextInt();
-			int devisor[]=new int[n];
-			System.out.println("Enter the divisor bit by bit:");
-			for(int i=0;i<n;i++) 
-			devisor[i]=s.nextInt();
-			int remainder[]=divide(data,devisor);
-			System.out.println("\n CRC code generated is:");
-			for(int i=0;i<data.length;i++)
-			System.out.println(data[i]);
-			for(int i=0;i<remainder.length;i++)
-			System.out.println(remainder[i]);
-			System.out.println();
-			int sent_data[]=new int[data.length+remainder.length-1];
-			System.out.println("Enter the data to be sent:");
-			for(int i=0;i<sent_data.length;i++)
-			sent_data[i]=s.nextInt();
-			receive(sent_data,devisor);
-			}
-	static int[] divide(int old_data[],int devisor[]){
-		int remainder[],i;
-		int data[]=new int[old_data.length+devisor.length];
-		System.arraycopy(old_data,0,data,0,old_data.length);
-		System.out.println("Message bits after appendinbg divcisor_length-1 0's:");
-		for( i=0;i<data.length-1;i++)
-		System.out.println(data[i]);
-		remainder=new int [devisor.length];
-		System.arraycopy(data,0,remainder,0,devisor.length);
-		for( i=0;i<old_data.length;i++){
-		if(remainder[0]==1){
-		for(int j=1;j<devisor.length;j++){
-		remainder[j-1]=exor(remainder[j],devisor[j]);
-	}
-	}
-	else {
-		for(int j=1;j<devisor.length;j++)
-		remainder[j-1]=exor(remainder[j],0);
-		}
-		remainder[devisor.length-1]=data[i+devisor.length];
-		}
-		return remainder;
-		}
-		static int exor(int a,int b){
-		if(a==b)
-		return 0;
-
+public class cr{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int n;
+System.out.println("enter size of data:");
+n=s.nextInt();
+int data[]=new int[n];
+System.out.println("enter the data bit by bit:");
+for(int i=0;i<n;i++){
+data[i]=s.nextInt();
+}
+System.out.println("enter the sixe of divisor:");
+n=s.nextInt();
+int divisor[]=new int[n];
+System.out.println("enter the divisor,bit by bit ");
+for(int i=0;i<n;i++)
+divisor[i]=s.nextInt();
+int remainder[]=divide(data,divisor);
+System.out.println("\n the crc code generated is:");
+for(int i=0;i<data.length;i++)
+System.out.print(data[i]);
+for(int i=0;i<remainder.length-1;i++)
+System.out.print(remainder[i]);
+System.out.println();
+int sd[]=new int[data.length+remainder.length-1];
+System.out.println("enter the data to be sent");
+for(int i=0;i<sd.length;i++)
+sd[i]=s.nextInt();
+recieve(sd,divisor);
+}
+static int[] divide(int od[],int divisor[])
+{
+int remainder[],i;
+int data[]=new int[od.length+divisor.length];
+System.arraycopy(od,0,data,0,od.length);
+System.out.println("message bits after appending divisor length-1 0's:");
+for(i=0;i<data.length-1;i++)
+System.out.println(data[i]);
+remainder=new int[divisor.length];
+System.arraycopy(data,0,remainder,0,divisor.length);
+for(i=0;i<od.length;i++)
+{
+if(remainder[0]==1)
+{
+for(int j=1;j<divisor.length;j++)
+{
+remainder[j-1]=exor(remainder[j],divisor[j]);
+}
+}
+else{
+for(int j=1;j<divisor.length;j++)
+remainder[j-1]=exor(remainder[j],0);
+}
+remainder[divisor.length-1]=data[i+divisor.length];
+}
+return remainder;
+}
+static int exor(int a,int b)
+{
+if(a==b)
+return 0;
 return 1;
 }
-static void receive(int data[],int devisor[]){
-int remainder[]=divide(data,devisor);
-for(int i=0;i<remainder.length;i++){
-if(remainder[i] !=0) {
-System.out.println("Thereis an error is recived data......");
+static void recieve(int data[],int divisor[]){int
+remainder[]=divide(data,divisor);
+for(int i=0; i<remainder.length;i++)
+{
+if (remainder[i]!=0)
+{
+System.out.println("there is an error in recived data.....");
 return;
 }
 }
-System.out.println("Data was received without any error");
+System.out.println("data was recived without error");
 }
 }
+
 
 
